cb8330f4b36b050c69eadbe4867f6dbe24140c2d
Mona B <web25Lucius@gmail.com>
2019-10-18 14:12:25 -0600

    reduce() last question

--- challenges/functions.js
+++ challenges/functions.js
@@ -50,8 +50,8 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: 
+// Explanation:  You can pull information from the country to the city, but not the other way around.  
+//Sort of like a crochet loop, you pull down- not up.   
 
 
 const external = "I'm outside the function";
